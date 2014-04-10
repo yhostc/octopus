f413472c0cd3e824da4d3dbeee98fd0f9c202cd2
yhostc <yhostc@gmail.com>
2014-04-10 18:11:22 +0800

    update dianping demo

--- example/dianping.js
+++ example/dianping.js
@@ -1,7 +1,7 @@
 var claw = require('../').Claw;
 
 var idx = 0;
-var zhuna = new claw({
+var dianping = new claw({
 	options: {
 		cache: true,
 		redis: {
@@ -14,19 +14,13 @@ var zhuna = new claw({
 		callback: function (error, window) {
 			if (window && window.$) {
 				var $ = window.$;
-				/*
-				.filter(function () {
-					console.log(this.href);
-					return this.href.match(/\/shop\/\d+$/)
-				})
-				 */
 				$('a[href]').each(function (idx, href) {
 					var link = $(href).attr('href');
 					try {
 						if (link.match(/^http/i)) {
-							zhuna.queue($(href).attr('href'));
+							dianping.queue($(href).attr('href'));
 						} else {
-							zhuna.queue('http://www.dianping.com' + link);
+							dianping.queue('http://www.dianping.com' + link);
 						}
 					} catch (e) {
 						console.error(link);
@@ -55,21 +49,21 @@ var zhuna = new claw({
 
 // loging
 var fet = 0;
-zhuna.on('fetch', function (data) {
+dianping.on('fetch', function (data) {
 	console.log('->fetch:', data.url, ', fet:', (fet++));
 	console.log('->remain:', data.remain, ', loading:', data.loading);
 });
 
 var err = 0;
-zhuna.on('errors', function (errors) {
+dianping.on('errors', function (errors) {
 	console.log('->errors:', errors, ', err:', (err++));
 });
 
 var war = 0;
-zhuna.on('warning', function (status, info) {
+dianping.on('warning', function (status, info) {
 	console.log('->status:', status, ', info:', info, ', war:', (war++));
 });
 
-zhuna.on('complete', function (info) {
+dianping.on('complete', function (info) {
 	console.log(info);
 });
